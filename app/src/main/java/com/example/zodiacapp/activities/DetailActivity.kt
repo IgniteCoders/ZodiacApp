7492e494334e65ffd9b395e5c5727f86ac30b37c
IgniteCoders <theboom.kajas@gmail.com>
2025-04-01 17:56:18 +0200

    We added the SessionManager to store the favorite horoscope.

--- app/src/main/java/com/example/zodiacapp/activities/DetailActivity.kt
+++ app/src/main/java/com/example/zodiacapp/activities/DetailActivity.kt
@@ -1,23 +1,31 @@
 package com.example.zodiacapp.activities
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.ImageView
 import android.widget.TextView
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.zodiacapp.R
+import com.example.zodiacapp.data.Horoscope
 import com.example.zodiacapp.data.HoroscopeProvider
+import com.example.zodiacapp.utils.SessionManager
+
 
 class DetailActivity : AppCompatActivity() {
 
     lateinit var nameTextView: TextView
     lateinit var datesTextView: TextView
     lateinit var iconImageView: ImageView
+
+    lateinit var session: SessionManager
+    lateinit var horoscope: Horoscope
+    var isFavorite = false
+    lateinit var favoriteMenuItem: MenuItem
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,37 +37,66 @@ class DetailActivity : AppCompatActivity() {
             insets
         }
 
+        session = SessionManager(this)
+
         nameTextView = findViewById(R.id.nameTextView)
         datesTextView = findViewById(R.id.datesTextView)
         iconImageView = findViewById(R.id.iconImageView)
 
         val id = intent.getStringExtra("HOROSCOPE_ID")!!
 
-        val horoscope = HoroscopeProvider.getById(id)!!
+        horoscope = HoroscopeProvider.getById(id)!!
+
+        isFavorite = session.getFavoriteHoroscope() == horoscope.id
 
         nameTextView.setText(horoscope.name)
         datesTextView.setText(horoscope.dates)
         iconImageView.setImageResource(horoscope.icon)
     }
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.activity_detail_menu, menu)
+
+        favoriteMenuItem = menu.findItem(R.id.menu_favorite)
+        setFavoriteIcon()
+
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             R.id.menu_favorite -> {
-                Toast.makeText(this, "Favorito", Toast.LENGTH_SHORT).show()
+                if (isFavorite) {
+                    session.setFavoriteHoroscope("")
+                } else {
+                    session.setFavoriteHoroscope(horoscope.id)
+                }
+                isFavorite = !isFavorite
+                setFavoriteIcon()
+
                 return true
             }
             R.id.menu_share -> {
-                Toast.makeText(this, "Compartir", Toast.LENGTH_SHORT).show()
+                val sendIntent = Intent()
+                sendIntent.setAction(Intent.ACTION_SEND)
+                sendIntent.putExtra(Intent.EXTRA_TEXT, "This is my text to send.")
+                sendIntent.setType("text/plain")
+
+                val shareIntent = Intent.createChooser(sendIntent, null)
+                startActivity(shareIntent)
                 return true
             }
             else -> {
                 return super.onOptionsItemSelected(item)
             }
+        }
+    }
+
+    fun setFavoriteIcon() {
+        if (isFavorite) {
+            favoriteMenuItem.setIcon(R.drawable.ic_favorite_selected)
+        } else {
+            favoriteMenuItem.setIcon(R.drawable.ic_favorite)
         }
     }
 }
